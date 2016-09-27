27856db59f59653ac54f53b3a5aa104d018b26b1
KevinSia <kevinsiakeemun@gmail.com>
2016-09-27 12:01:08 +0800

    amended seed file to not create dup category

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,5 +13,5 @@ alias Rumbl.Repo
 alias Rumbl.Category
 
 for category <- ~w(Action Drama Romance Sci-Fi Comedy) do
-  Repo.insert!(%Category{name: category})
+  Repo.get_by(Category, name: category) || Repo.insert!(%Category{name: category})
 end
