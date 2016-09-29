9cad3880146cc536a46587376ed7ff4a38dd666f
KevinSia <kevinsiakeemun@gmail.com>
2016-09-29 11:20:12 +0800

    test alphabetical/1 in Category

--- test/models/category_test.exs
+++ test/models/category_test.exs
@@ -15,4 +15,15 @@ defmodule Rumbl.CategoryTest do
     changeset = Category.changeset(%Category{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  # a test with side effect (inserting into repo)
+  test "alphabetical/1 orders by name" do
+    for name <- ~w(c a b) do
+      Repo.insert!(%Category{name: name})
+    end
+
+    query = Category |> Category.alphabetical()
+    query = from c in query, select: c.name
+    assert Repo.all(query) == ~w(a b c)
+  end
 end
